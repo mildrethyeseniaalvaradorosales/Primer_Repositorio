e4761cb64c283a45426a2c7e5e5f588b21edbd4b
mildrethyeseniaalvaradorosales <mildrethalvaradorosales1@gmail.com>
2024-10-21 09:31:15 -0600

    Add files via upload

---
+++ P00Presentación.php
@@ -0,0 +1,12 @@
+<?php
+/*CBTIS 89
+P00Presentación
+Programa contiene mis datos personales
+Mildreth Yesenia Alvarado Rosales
+3°A Programación Matutino
+*/
+echo "Mi nombre es Mildreth Yesenia Alvarado Rosales";
+echo"<br>","Estudio en el CBTIS 89";
+echo"<br>","En tercer semestre de la carrera de Programación A Matutino";
+echo "<br>","Vivo en la ciudad de Durango,Dgo.";
+?>
