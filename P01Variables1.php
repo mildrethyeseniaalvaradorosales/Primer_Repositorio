0e64717201925ac6a048b66f6a5327acbec71403
mildrethyeseniaalvaradorosales <mildrethalvaradorosales1@gmail.com>
2024-10-21 09:10:09 -0600

    Add files via upload

---
+++ P01Variables1.php
@@ -0,0 +1,23 @@
+<?php 
+/*CBTIS 89
+Programa que declara variables y nos da sueldo ,nombre,dia e imprimey une cadena de texto.
+Mildreth Yesenia Alvarado Rosales
+3°A Programación Matutino
+*/
+$dia = 24; //Se declara una variable de tipo integer.
+$sueldo = 758.43; // Se declara una variable de tipo double.
+$nombre = "Juan"; //Se declara una variable de tipo string.
+$exite = true; //Se declara una variable boolean.
+echo "Variable entera:";
+echo $dia;echo "<br>";
+echo "Variable double:";
+echo $sueldo;echo "<br>";
+echo "Variable string:";
+echo $nombre;echo "<br>";
+echo "Variable boolean:";
+echo $exite;
+echo "<br>";
+$cadena1="Hola";
+$cadena2="Mundo";
+echo $cadena1."   ".$cadena2;
+?>
