fc5545b81a0d3226e2fc96e58dadac5c386b7c5e
mildrethyeseniaalvaradorosales <mildrethalvaradorosales1@gmail.com>
2024-11-08 10:20:42 -0600

    P39Array19.php

--- Unidad3/P39Array19.php
+++ Unidad3/P39Array19.php
@@ -4,7 +4,7 @@ Mildreth Yesenia Alvarado Rosales
 P39Array19.php 
 Programa que almacena información de 6 personas y dependiendo de su edad los contabiliza en diversos arreglos de acuerdo a las siguientes condiciones:
 Si la persona es menor a 18 años se debe colocar 1 en el arreglo $Grupo1 y en los otros dos colocar un 0.
-Si la persona tiene entre 19 y 49 años se debe colocar un 1 en el arreglo $Grupo2 y en los otros colocar un 0.
+Si la persona tiene entre 18 y 49 años se debe colocar un 1 en el arreglo $Grupo2 y en los otros colocar un 0.
 Si la persona tiene 50 años o más se debe colocar un 1 en el arreglo $Grupo3 y en los otros colocar un 0.
 3°A Programación T.M.
 */
@@ -21,7 +21,7 @@ for ($i = 0; $i < count($Edad); $i++) {
     if ($Edad[$i] < 18) {
         $Grupo1[$i] = 1;
     } 
-    elseif ($Edad[$i] >= 19 && $Edad[$i] <= 49) {
+    elseif ($Edad[$i] >= 18 && $Edad[$i] <= 49) {
         $Grupo2[$i] = 1; 
     } 
     elseif ($Edad[$i] >= 50) {
